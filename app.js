784bdd023bb0f7097ed4fb18b8f3fc7ca400ba44
LandingsForYou <landingsforyou@gmail.com>
2026-04-01 00:43:05 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const burger = document.getElementById("burger");
 const mobileMenu = document.getElementById("mobileMenu");
 
 burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
   mobileMenu.classList.toggle("active");
 });
 
 mobileMenu.querySelectorAll("a").forEach((link) => {
   link.addEventListener("click", () => {
+    burger.classList.remove("active");
     mobileMenu.classList.remove("active");
   });
 });
